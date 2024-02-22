49e5fb2401832afe38a2b5d3d1da9b1d2993047b
Scott Pashley <scott.pashley@giantdigital.co.uk>
2024-02-22 14:22:08 +0000

    Rename app to GiantSearch from Search to avoid confusion and conflicts with other search apps.

--- search/views.py
+++
@@ -1,9 +0,0 @@
-from watson.views import SearchView as WatsonSearchView
-
-
-class SearchView(WatsonSearchView):
-    template_name = "search/results.html"
-
-    def get_queryset(self):
-        q = super().get_queryset()
-        return q
